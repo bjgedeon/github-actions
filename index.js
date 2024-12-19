900326beb44016551d6880559372c24217006383
bjorngedeon <bjoern.gedeon@gmail.com>
2024-12-19 16:14:09 +0100

    test github actions

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 import { generate } from 'random-words';
 import chalk from 'chalk';
 
-console.log("Hello World");
-
 const word1 = generate({ exactly: 1, minLength: 10, wordsPerString: 1 })[0];
 const word2 = generate();
 
